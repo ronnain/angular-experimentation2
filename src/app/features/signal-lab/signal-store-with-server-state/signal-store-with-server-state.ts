51acc9bbaf189ab2f758041d144507c2fbe3f7a6
Romain <rgeffrault@dood.com>
2025-06-21 11:05:49 +0200

    try withQuery

--- src/app/features/signal-lab/signal-store-with-server-state/signal-store-with-server-state.ts
+++ src/app/features/signal-lab/signal-store-with-server-state/signal-store-with-server-state.ts
@@ -22,12 +22,20 @@ import {
   signalStoreFeature,
   SignalStoreFeatureResult,
   StateSignals,
+  withHooks,
   withMethods,
   withProps,
   withState,
   WritableStateSource,
 } from '@ngrx/signals';
-import { of } from 'rxjs';
+import { from, lastValueFrom, of } from 'rxjs';
+import {
+  assertUniqueStoreMembers,
+  InnerSignalStore,
+  SignalsDictionary,
+  STATE_SOURCE,
+} from './inner-signal-store';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 type Merge<T, U> = T & U;
 
@@ -68,6 +76,20 @@ type UserTest = {
   email: string;
 };
 
+const r = rxResource({
+  params: () => ({
+    page: 1,
+    pageSize: 10,
+  }),
+  stream: ({ params }) => {
+    return of<UserTest>({
+      id: '1',
+      name: 'John Doe',
+      email: 'john.doe@a.com',
+    });
+  },
+});
+
 const storeTest = signalStore(
   withState({
     pagination: {
@@ -75,77 +97,146 @@ const storeTest = signalStore(
       pageSize: 10,
     },
   }),
-  withQuery((storeData) => {
-    storeData.test;
-    return {
-      resourceName: 'users',
-      resource: resource<UserTest>({
-        params: storeData.stateSignals.pagination(),
-        loader: (pagination) => {
-          return of<UserTest>({
-            id: '1',
-            name: 'John Doe',
-            email: 'john.doe@a.com',
-          });
-        },
-      }),
-    };
-  })
+  withQuery((store) => ({
+    //         ^?
+    resource: rxResource({
+      params: store.pagination,
+      stream: ({ params }) => {
+        return of<UserTest>({
+          id: '1',
+          name: 'John Doe',
+          email: 'john.doe@a.com',
+        });
+      },
+    }),
+    resourceName: 'users',
+  })),
+  withHooks((store) => ({
+    onInit: () => {
+      const test = store.users;
+
+      console.log('Store initialized', store);
+    },
+    onDestroy: () => {
+      console.log('Store destroyed');
+    },
+  }))
 );
 
 const testImpl = inject(storeTest);
 // testImpl.entities()[0].uiStatus?.getAll.isLoading;
 // testImpl.entitiesActionsEvents.getAll.subscribe((event) => console.log(event));
 
+const queryTest = withQuery(() => ({
+  //         ^?
+  resource: resource({
+    loader: () => {
+      return lastValueFrom(
+        of<UserTest>({
+          id: '1',
+          name: 'John Doe',
+          email: 'john.doe@a.com',
+        })
+      );
+    },
+  }),
+  resourceName: 'users',
+}));
+
 function withQuery<
-  State extends object,
-  ResourceName extends string,
-  StateI,
-  Input extends {
-    state: StateI;
-    props: object;
-    methods: Methods;
-  },
-  Incoming extends Prettify<
-    StateSignals<Input['state']> &
-      Input['props'] &
-      Input['methods'] &
-      WritableStateSource<Prettify<Input['state']>>
-  >,
-  Methods extends Record<string, Function>
+  Input extends SignalStoreFeatureResult,
+  const ResourceName extends string,
+  State
 >(
-  fn: (store: Incoming) => {
+  methodsFactory: (
+    store: Prettify<
+      StateSignals<Input['state']> &
+        Input['props'] &
+        Input['methods'] & // todo remove methods ?
+        WritableStateSource<Prettify<Input['state']>>
+    >
+  ) => {
     resourceName: ResourceName;
     resource: ResourceRef<State>;
-    initialValue: NoInfer<State> | undefined;
   }
-) {
-  return (store: T) => {
-    const { resourceName, resource, initialValue } = fn(store);
-    const initialResourceState: ResourceData<State> = {
-      value: initialValue,
-      status: {
-        status: 'local',
-        isLoading: false,
-        error: undefined,
-      },
-    };
-  };
+): SignalStoreFeature<
+  Input,
+  { state: { [key: ResourceName]: { test: true } }; props: {}; methods: {} }
+> {
+  // ): SignalStoreFeature<Input, { state: {[key: ResourceName]: State}; props: {}; methods: {} }> {
+  return (store) => {
+    // const methods = methodsFactory({
+    //   [STATE_SOURCE]: store[STATE_SOURCE],
+    //   ...store.stateSignals,
+    //   ...store.props,
+    //   ...store.methods,
+    // });
+    // assertUniqueStoreMembers(store, Reflect.ownKeys(methods));
 
-  // return signalStoreFeature(
-  //   withState({[resourceName]: initialResourceState}),
-  //   withProps((store) => ({
-  //     [`_${resourceName}Effect`]: effect(() => {
-  //         patchState(store, (state) => ({
-  //           [resourceName]: {
-  //             value: resource.hasValue() ? resource.value() : state[resourceName].value,
-  //             status: {
-  //               isLoading: resource.isLoading(),
-  //               status: resource.status(),
-  //               error: resource.error(),
-  //             },
-  //           } satisfies ResourceData<State>,
-  //         }));
-  //     }),
-  //   })))
+    return {
+      ...store,
+      // methods: { ...store.methods, ...methods },
+    } as unknown as InnerSignalStore<{ [key: ResourceName]: { test: true } }>;
+  };
+}
+
+// return signalStoreFeature(
+//   withState({[resourceName]: initialResourceState}),
+//   withProps((store) => ({
+//     [`_${resourceName}Effect`]: effect(() => {
+//         patchState(store, (state) => ({
+//           [resourceName]: {
+//             value: resource.hasValue() ? resource.value() : state[resourceName].value,
+//             status: {
+//               isLoading: resource.isLoading(),
+//               status: resource.status(),
+//               error: resource.error(),
+//             },
+//           } satisfies ResourceData<State>,
+//         }));
+//     }),
+//   })))
+
+function withQuery2<
+  Input extends SignalStoreFeatureResult,
+  const ResourceName extends string,
+  State
+>(
+  methodsFactory: (
+    store: Prettify<
+      StateSignals<Input['state']> &
+        Input['props'] &
+        Input['methods'] & // todo remove methods ?
+        WritableStateSource<Prettify<Input['state']>>
+    >
+  ) => {
+    resourceName: ResourceName;
+    resource: ResourceRef<State>;
+  }
+): SignalStoreFeature<
+  Input,
+  { state: { [key in ResourceName]: { test: true } }; props: {}; methods: {} }
+> {
+  return ((store) => {
+    const { resource, resourceName } = methodsFactory(
+      store as unknown as StateSignals<Input['state']> &
+        Input['props'] &
+        Input['methods'] & // todo remove methods ?
+        WritableStateSource<Prettify<Input['state']>>
+    );
+    return signalStoreFeature(
+      withState({
+        [resourceName]: {
+          value: undefined as State | undefined,
+          status: {
+            isLoading: false,
+            status: 'idle',
+            error: undefined,
+          } satisfies ResourceStatusData,
+        },
+      })
+    );
+  }) as ;
+
+  // ): SignalStoreFeature<Input, { state: {[key: ResourceName]: State}; props: {}; methods: {} }> {
 }
