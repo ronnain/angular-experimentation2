bad27b3d9e8a0182c41b17097020e31e298ae3e8
Romain <romain.geffrault@gmail.com>
2025-06-24 22:10:35 +0200

    improve example

--- src/app/features/signal-lab/signal-store-with-server-state/test.store.ts
+++ src/app/features/signal-lab/signal-store-with-server-state/test.store.ts
@@ -2,6 +2,12 @@ import { signalStore, withHooks, withMethods, withState } from '@ngrx/signals';
 import { withQueryById } from './signal-store-with-server-state';
 import { resourceById } from './resource-by-id-signal-store';
 
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
 export const testStore = signalStore(
   {
     providedIn: 'root',
@@ -13,21 +19,25 @@ export const testStore = signalStore(
     },
     selectedUserId: '5' as string | undefined,
   }),
-  withQueryById((store) => {
-    return {
-      resourceName: 'usersById',
-      resource: resourceById({
-        params: store.selectedUserId,
-        loader: ({ params }) => {
-          console.log('params', params);
-          return Promise.resolve({
-            id: '1',
-            name: 'John Doe',
-            email: 'a@a.fr',
-          });
-        },
-        identifier: (params) => params,
-      }),
-    };
-  })
+  withQueryById((store) => ({
+    resourceName: 'usersById',
+    resource: resourceById({
+      params: store.selectedUserId,
+      loader: ({ params }) => {
+        console.log('params', params);
+        return new Promise<User>((resolve) => {
+          setTimeout(() => {
+            resolve({
+              id: '1',
+              name: 'John Doe',
+              email: 'a@a.fr',
+            });
+          }, 3000);
+        });
+      },
+      identifier: (params) => params,
+    }),
+  }))
 );
+
+// todo checker pourquoi il n'y a pas de value quand ça charge
