f28c435cdebf98bd0e2c5102fa524891f5eccd9a
Romain <rgeffrault@dood.com>
2025-07-08 09:09:59 +0200

    improve mutation

---
+++ src/app/features/signal-lab/signal-store-with-server-state/with-mutation.spec.ts
@@ -0,0 +1,109 @@
+import { signalStoreFeature, SignalStoreFeature } from '@ngrx/signals';
+import { Equal } from '../../../../../test-type';
+import { withQuery } from './with-query';
+import { lastValueFrom, of } from 'rxjs';
+import { resource } from '@angular/core';
+import { withMutation } from './with-mutation';
+import { id } from 'fp-ts/lib/Refinement';
+import { ObjectDeepPath } from './object-deep-path-mapper.type';
+import {
+  AccessTypeObjectPropertyByDottedPath,
+  DottedPathPathToTuple,
+} from './access-type-object-property-by-dotted-path.type';
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  address?: {
+    street: string;
+  };
+};
+
+type InferSignalStoreFeatureReturnedType<
+  T extends SignalStoreFeature<any, any>
+> = T extends SignalStoreFeature<any, infer R> ? R : never;
+
+it('Should be well typed', () => {
+  const multiplesWithQueryAndMutation = signalStoreFeature(
+    withQuery('user', () =>
+      resource({
+        params: () => '5',
+        loader: ({ params }) => {
+          return lastValueFrom(
+            of({
+              id: params,
+              name: 'John Doe',
+              email: 'test@a.com',
+            } satisfies User as User)
+          );
+        },
+      })
+    ),
+    withQuery('users', () =>
+      resource({
+        params: () => '5',
+        loader: ({ params }) => {
+          return lastValueFrom(
+            of([
+              {
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              },
+            ] satisfies User[])
+          );
+        },
+      })
+    ),
+    withMutation('updateUser', () => ({
+      mutation: resource({
+        params: () => '5',
+        loader: ({ params }) => {
+          return lastValueFrom(
+            of({
+              id: params,
+              name: 'Updated User',
+              email: 'er@d',
+            } satisfies User)
+          );
+        },
+      }),
+      queries: {
+        user: {
+          reload: {
+            onMutationError: true,
+          },
+
+          optimisticPatch: {
+            'address.street': () => 'true',
+          },
+        },
+        users: {
+          optimistic: ({ queryResource, mutationResource }) => {
+            return queryResource.value();
+          },
+        },
+      },
+    }))
+  );
+
+  type ResultTypeMultiplesQuery = InferSignalStoreFeatureReturnedType<
+    typeof multiplesWithQueryAndMutation
+  >;
+
+  type ExpectThePropsToHaveARecordWithMultipleQueryNameAndHisType = Expect<
+    Equal<keyof ResultTypeMultiplesQuery['props']['__query'], 'user' | 'users'>
+  >;
+});
+
+type testUserPath = ObjectDeepPath<User & {}>;
+
+const test: {
+  [key in testUserPath]?: AccessTypeObjectPropertyByDottedPath<
+    User,
+    DottedPathPathToTuple<key>
+  >;
+} = {
+  'address.street': 'test',
+};
--- src/app/features/signal-lab/signal-store-with-server-state/with-mutation.ts
+++ src/app/features/signal-lab/signal-store-with-server-state/with-mutation.ts
@@ -14,41 +14,48 @@ import {
   ResourceData,
   ResourceStatusData,
 } from './signal-store-with-server-state';
+import { Merge } from '../../../util/types/merge';
+import { MergeObject } from './util.type';
+import { ObjectDeepPath } from './object-deep-path-mapper.type';
+import {
+  AccessTypeObjectPropertyByDottedPath,
+  DottedPathPathToTuple,
+} from './access-type-object-property-by-dotted-path.type';
 
 // todo withQuery... faire un state initial qui représente l'état et sera muté par la mutation, et préserver la réponse de la query dans un champs spécifique readonly
 
 // ! pas ajouter de computed/selector, juste appliquer la mutation et l'exposer via un computed ?
 // objectif permettre de faire des mutations optimistes, et de gérer les erreurs
-const mutation = withMutations((store) => {
-  return {
-    // todo lié un mutation à un state ? dans le cas où on veut faire de l'optimistic update ?
-    updateTodo: (id: string, todo: Todo) => resource({}), // expose une fonction dans methods
-    updateTodo2: resource({
-      params: store.updatedTodo, // signal
-      // loader ...
-    }), // n'expose pas de fonction, juste un resource (qui elle peut être peut-être déclarative)
-    update3: {
-      method?: (id: string, todo: Todo) => resource({}),
-      stateTargetedPath?: (state, params) => state.users[params.id]  // will perform an optimistic update on the state, marche pas si c'est dans un array
-      optimistic?: true,
-      optimisticResolver??: (params: !StateTargeted, stateTargeted: StateTargeted) => StateTargeted
-    },
-    update4: {
-      method?: (id: string, todo: Todo) => resource({}),
-      mutationChange: (mutationResource: ResourceMutation) => {
-        if(mutationResource.isLoading) {
-          // do something
-        }
-      },
-    }
-  };
-});
+// const mutation = withMutations((store) => {
+//   return {
+//     // todo lié un mutation à un state ? dans le cas où on veut faire de l'optimistic update ?
+//     updateTodo: (id: string, todo: Todo) => resource({}), // expose une fonction dans methods
+//     updateTodo2: resource({
+//       params: store.updatedTodo, // signal
+//       // loader ...
+//     }), // n'expose pas de fonction, juste un resource (qui elle peut être peut-être déclarative)
+//     update3: {
+//       method?: (id: string, todo: Todo) => resource({}),
+//       stateTargetedPath?: (state, params) => state.users[params.id]  // will perform an optimistic update on the state, marche pas si c'est dans un array
+//       optimistic?: true,
+//       optimisticResolver??: (params: !StateTargeted, stateTargeted: StateTargeted) => StateTargeted
+//     },
+//     update4: {
+//       method?: (id: string, todo: Todo) => resource({}),
+//       mutationChange: (mutationResource: ResourceMutation) => {
+//         if(mutationResource.isLoading) {
+//           // do something
+//         }
+//       },
+//     }
+//   };
+// });
 export function withMutation<
   Input extends SignalStoreFeatureResult,
   const MutationName extends string,
-  State extends object | undefined
+  MutationState extends object | undefined
 >(
-  resourceName: MutationName,
+  mutationName: MutationName,
   queryFactory: (
     store: Prettify<
       StateSignals<Input['state']> &
@@ -56,14 +63,78 @@ export function withMutation<
         Input['methods'] & // todo remove methods ?
         WritableStateSource<Prettify<Input['state']>>
     >
-  ) => ResourceRef<State>
+  ) =>
+    | ResourceRef<MutationState>
+    | MergeObject<
+        {
+          mutation: ResourceRef<MutationState>;
+          queries: Input['props'] extends { __query: infer Q }
+            ? {
+                [key in keyof Q]: {
+                  /**
+                   * Will update the query state with the mutation data.
+                   */
+                  optimistic?: MutationState extends Q[key]
+                    ? boolean
+                    : (data: {
+                        queryResource: ResourceRef<Q[key]>;
+                        mutationResource: ResourceRef<MutationState>;
+                      }) => Q[key];
+                  /**
+                   * Will reload the query when the mutation is in a specific state.
+                   * If the query is loading, it will not reload.
+                   */
+                  reload?:
+                    | false
+                    | {
+                        onMutationError?: boolean;
+                        onMutationResolved?: boolean;
+                        onMutationLoading?: boolean;
+                        onMutationIdle?: boolean;
+                      };
+                  /**
+                   * Will patch the query specific state with the mutation data.
+                   * If the query is loading, it will not patch.
+                   * If the mutation data is not compatible with the query state, it will not patch.
+                   */
+                  optimisticPatch?: {
+                    [queryPatchPath in ObjectDeepPath<
+                      Q[key] & {}
+                    >]?: AccessTypeObjectPropertyByDottedPath<
+                      Q[key] & {},
+                      DottedPathPathToTuple<queryPatchPath>
+                    > extends infer TargetedType
+                      ? MutationState extends TargetedType
+                        ? true
+                        : (data: {
+                            queryResource: NoInfer<ResourceRef<Q[key]>>;
+                            mutationResource: NoInfer<
+                              ResourceRef<MutationState>
+                            >;
+                            targetedState: TargetedType;
+                          }) => TargetedType
+                      : never;
+                  };
+                };
+              }
+            : never;
+        },
+        {}
+      >
 ): SignalStoreFeature<
   Input,
   {
-    state: { [key in MutationName]: ResourceData<State> };
-    props: {
-      [key in `_${MutationName}Effect`]: EffectRef;
-    };
+    state: {};
+    props: Merge<
+      {
+        [key in MutationName]: ResourceData<MutationState>;
+      },
+      {
+        __mutation: {
+          [key in MutationName]: MutationState;
+        };
+      }
+    >;
     methods: {};
   }
 > {
@@ -78,7 +149,7 @@ export function withMutation<
     return signalStoreFeature(
       withState({
         [resourceName]: {
-          value: resource.value() as State | undefined,
+          value: resource.value() as MutationState | undefined,
           status: {
             isLoading: false,
             isLoaded: false,
@@ -94,7 +165,7 @@ export function withMutation<
             [resourceName]: {
               value: resource.hasValue()
                 ? resource.value()
-                : (state[resourceName].value as State),
+                : (state[resourceName].value as MutationState),
               status: {
                 isLoading: resource.isLoading(),
                 isLoaded: resource.status() === 'resolved',
@@ -102,7 +173,7 @@ export function withMutation<
                 status: resource.status(),
                 error: resource.error(),
               },
-            } satisfies ResourceData<State>,
+            } satisfies ResourceData<MutationState>,
           }));
         }),
       }))
@@ -111,7 +182,7 @@ export function withMutation<
   }) as unknown as SignalStoreFeature<
     Input,
     {
-      state: { [key in MutationName]: ResourceData<State> };
+      state: { [key in MutationName]: ResourceData<MutationState> };
       props: {
         [key in `_${MutationName}Effect`]: EffectRef;
       };
