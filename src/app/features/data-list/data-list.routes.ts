8e3542a063739906c69b7ac5d2aa1bfb55d4dd90
Romain <rgeffrault@dood.com>
2025-08-03 08:07:33 +0200

    clean project

--- src/app/features/data-list/data-list.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { DataListComponent } from './data-list.component';
-
-export const dataListRoutes: Route[] = [
-  {
-    path: 'data-list',
-    component: DataListComponent,
-  },
-];
--- src/app/features/signal-lab/signal-lab.routes.ts
+++ src/app/features/signal-lab/signal-lab.routes.ts
@@ -3,24 +3,6 @@ import { Route } from '@angular/router';
 export const signalLabRoutes: Route[] = [
   {
     path: 'signal-lab',
-    children: [
-      {
-        path: 'resource-by-group',
-        loadComponent: () => import('./resource-by-group/resource-by-group.ng'),
-      },
-      {
-        path: 'resource-store',
-        loadComponent: () => import('./resource-store/resource-store.ng'),
-      },
-      {
-        path: 'pipe',
-        loadComponent: () => import('./pipe/my-signal-store.ng'),
-      },
-      {
-        path: 'signal-store-with-query',
-        loadComponent: () =>
-          import('./signal-store-with-server-state/view.component'),
-      },
-    ],
+    children: [],
   },
 ];
